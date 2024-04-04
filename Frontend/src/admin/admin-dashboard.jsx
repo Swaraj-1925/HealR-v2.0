5b34a9f946609600df4365ee230a7420a5d55b92
Swaraj-1925 <swarajgaikwad1925@gmail.com>
2024-04-05 00:45:49 +0530

    admin signup page,dashboard layout 50% done

---
+++ Frontend/src/admin/admin-dashboard.jsx
@@ -0,0 +1,32 @@
+function Admin_dashboard() {
+  return (
+    <>
+      <div className="adminDashboard-conatiner">
+        <div className="adminDahsboard-gridContainer1">
+          <div className="adminDahsboard-gridContainer1-item adminDashboard-statusContainer">
+            <h1>Active Patient</h1>
+              <div className="statusContainerNumber">10</div>
+
+          </div>
+          <div className="adminDahsboard-gridContainer1-item adminDashboard-statusContainer">
+          <h1>Active Doctor</h1>
+              <div className="statusContainerNumber">10</div>
+          </div>
+          <div className="adminDahsboard-gridContainer1-item adminDashboard-statusContainer">
+          <h1>Staff</h1>
+              <div className="statusContainerNumber">10</div>
+          </div>
+          <div className="adminDahsboard-gridContainer1-item adminDashboard-statusContainer">
+          <h1>Appoinments</h1>
+              <div className="statusContainerNumber">10</div>
+          </div>
+        </div>
+        <div className="adminDahsboard-gridContainer2">
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Admin_dashboard; 
